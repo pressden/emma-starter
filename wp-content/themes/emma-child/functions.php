832665bb706cf402b7cfd6cd1cfaaec01228842c
D.S. Webster <dswebs.me@gmail.com>
2019-12-18 12:47:20 -0500

    Add emma-child to the repo

---
+++ wp-content/themes/emma-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Emma Child functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Emma Child
+ */
---
+++ wp-content/themes/emma-child/src/js/index.js
@@ -0,0 +1,4 @@
+import '../sass/style.scss'
+
+import '../../../emma/src/js/navigation';
+import '../../../emma/src/js/skip-link-focus-fix';
