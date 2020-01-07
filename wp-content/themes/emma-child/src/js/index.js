7ce7cbfba538f3e825fc03a69f81f4b92c8916f3
D.S. Webster <dswebs.me@gmail.com>
2020-01-07 17:03:33 -0500

    Simplify index.js using required.js

--- wp-content/themes/emma-child/src/js/index.js
+++ wp-content/themes/emma-child/src/js/index.js
@@ -1,5 +1,3 @@
 import '../sass/style.scss'
 
-import '../../../emma/src/js/navigation';
-import '../../../emma/src/js/skip-link-focus-fix';
-import '../../../emma/src/js/search';
+import '../../../emma/src/js/required';
