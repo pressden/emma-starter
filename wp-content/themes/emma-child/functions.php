ebb647b706f58fe042f15154c39a7fcccc3425ba
Eric Michel <45110062+eric-michel@users.noreply.github.com>
2020-04-09 16:53:23 -0400

    added color palette override function example

--- wp-content/themes/emma-child/functions.php
+++ wp-content/themes/emma-child/functions.php
@@ -6,3 +6,17 @@
  *
  * @package Emma Child
  */
+
+function emma_child_color_palette( $colors ) {
+  /* overrides various registered colors with new hex values */
+  //$colors['primary']['color'] = '#0073aa';
+  //$colors['primary-support']['color'] = '#D4F9F8';
+  //$colors['secondary']['color'] = '#663F46';
+  //$colors['secondary-support']['color'] = '#B8D8D8';
+  
+  /* removes a color from the editor palette */
+  //unset( $colors['accent'] );
+
+  return $colors;
+}
+add_filter ( 'emma_editor_color_palette', 'emma_child_color_palette' );
