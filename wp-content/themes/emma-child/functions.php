eaaffc63ff10934e8667791c90c391df70600758
Eric Michel <45110062+eric-michel@users.noreply.github.com>
2020-04-09 17:03:59 -0400

    removed wildcard comments

--- wp-content/themes/emma-child/functions.php
+++ wp-content/themes/emma-child/functions.php
@@ -8,13 +8,13 @@
  */
 
 function emma_child_color_palette( $colors ) {
-  /* overrides various registered colors with new hex values */
+  //overrides various registered colors with new hex values
   //$colors['primary']['color'] = '#0073aa';
   //$colors['primary-support']['color'] = '#D4F9F8';
   //$colors['secondary']['color'] = '#663F46';
   //$colors['secondary-support']['color'] = '#B8D8D8';
   
-  /* removes a color from the editor palette */
+  //removes a color from the editor palette
   //unset( $colors['accent'] );
 
   return $colors;
