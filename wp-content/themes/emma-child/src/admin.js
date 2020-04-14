7ab5e01a82e33fa08bff21f061976a20c4bf8873
D.S. Webster <dswebs.me@gmail.com>
2020-04-14 02:26:33 -0400

    Update remaining files for code splitting

---
+++ wp-content/themes/emma-child/src/admin.js
@@ -0,0 +1,9 @@
+/*--------------------------------------------------------------
+# Emma Child - Webpack Admin Entrypoint
+--------------------------------------------------------------*/
+
+// CSS
+import './sass/admin.scss';
+
+// JS 
+import '../../emma/src/js/emma-admin';
---
+++ wp-content/themes/emma-child/src/theme.js
@@ -0,0 +1,9 @@
+/*--------------------------------------------------------------
+# Emma Child - Webpack Theme Entrypoint
+--------------------------------------------------------------*/
+
+// CSS
+import './sass/theme.scss';
+
+// JS
+import '../../emma/src/js/emma-theme';
