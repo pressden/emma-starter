cb0057191b104cda6f34e5a19bf2089f90e1408c
Eric Michel <eric.r.michel@gmail.com>
2021-02-16 12:05:00 -0500

    installed scssphp and created proof of concept

--- wp-content/themes/emma-child/functions.php
+++ wp-content/themes/emma-child/functions.php
@@ -20,3 +20,56 @@ function emma_child_editor_color_palette_filter( $colors ) {
   return $colors;
 }
 add_filter ( 'emma_editor_color_palette', 'emma_child_editor_color_palette_filter' );
+
+use ScssPhp\ScssPhp\Compiler;
+function emma_child_compile_theme_css() {
+  require_once 'inc/vendor/scssphp/scss.inc.php';
+  $target_css = get_stylesheet_directory() . '/dist/theme.css';
+
+  $base_scss_files = array(
+    get_stylesheet_directory() . '/src/sass/theme',
+  );
+  $import_scss_files = apply_filters( 'emma_child_theme_scss', $base_scss_files );
+
+  $compiler = new Compiler();
+  $variables = apply_filters( 'emma_child_scss_variables', array() );
+  $compiler->setVariables( $variables );
+  
+  ob_start();
+  foreach( $import_scss_files as $scss_file ) { ?>
+    @import "<?php echo str_replace('\\', '/', $scss_file ); ?>";
+  <?php }
+  $import_scss = ob_get_clean();
+
+  $css = $compiler->compile( $import_scss );
+  if ( !empty( $css ) && is_string( $css ) ) {
+		file_put_contents( $target_css, $css );
+	}
+}
+add_action( 'wp_loaded', 'emma_child_compile_theme_css' );
+
+function emma_child_compile_admin_css() {
+  require_once 'inc/vendor/scssphp/scss.inc.php';
+  $target_css = get_stylesheet_directory() . '/dist/admin.css';
+
+  $base_scss_files = array(
+    get_stylesheet_directory() . '/src/sass/admin',
+  );
+  $import_scss_files = apply_filters( 'emma_child_admin_scss', $base_scss_files );
+
+  $compiler = new Compiler();
+  $variables = apply_filters( 'emma_child_scss_variables', array() );
+  $compiler->setVariables( $variables );
+  
+  ob_start();
+  foreach( $import_scss_files as $scss_file ) { ?>
+    @import "<?php echo str_replace('\\', '/', $scss_file ); ?>";
+  <?php }
+  $import_scss = ob_get_clean();
+
+  $css = $compiler->compile( $import_scss );
+  if ( !empty( $css ) && is_string( $css ) ) {
+		file_put_contents( $target_css, $css );
+	}
+}
+add_action( 'wp_loaded', 'emma_child_compile_admin_css' );
