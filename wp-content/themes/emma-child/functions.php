39cbd666d325d27c3c4bb09c128f9378782321e8
D.S. Webster <dswebs.me@gmail.com>
2021-07-07 10:02:26 -0400

    Clean up based on PHPCS recommendations

--- wp-content/themes/emma-child/functions.php
+++ wp-content/themes/emma-child/functions.php
@@ -7,16 +7,21 @@
  * @package Emma Child
  */
 
+/**
+ * Customize the editor color palette
+ *
+ * @param array $colors Editor palette colors.
+ */
 function emma_child_editor_color_palette_filter( $colors ) {
-  //overrides various registered colors with new hex values
-  //$colors['primary']['color'] = '#0073aa';
-  //$colors['primary-support']['color'] = '#D4F9F8';
-  //$colors['secondary']['color'] = '#663F46';
-  //$colors['secondary-support']['color'] = '#B8D8D8';
-  
-  //removes a color from the editor palette
-  //unset( $colors['accent'] );
+	// Override existing colors with values.
+	$colors['primary']['color']           = '#0073aa';
+	$colors['primary-support']['color']   = '#d4f9f8';
+	$colors['secondary']['color']         = '#663f46';
+	$colors['secondary-support']['color'] = '#b8d8d8';
 
-  return $colors;
+	// Use unset to remove colors from the editor palette.
+	// Example: unset( $colors['accent'] ).
+
+	return $colors;
 }
-add_filter ( 'emma_editor_color_palette', 'emma_child_editor_color_palette_filter' );
+add_filter( 'emma_editor_color_palette', 'emma_child_editor_color_palette_filter' );
