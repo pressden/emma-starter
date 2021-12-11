e25974dba5cbc4bde4d5ef2c139da3c06f712230
D.S. Webster <dswebs.me@gmail.com>
2021-12-11 15:56:31 -0500

    Remove emma-child in favor of using the standalone repo

--- wp-content/themes/emma-child/src/admin.js
+++
@@ -1,9 +0,0 @@
-/*--------------------------------------------------------------
-# Emma Child - Webpack Admin Entrypoint
---------------------------------------------------------------*/
-
-// CSS
-import './sass/admin.scss';
-
-// JS
-import '../../emma/src/js/emma-admin';
--- wp-content/themes/emma-child/src/theme.js
+++
@@ -1,9 +0,0 @@
-/*--------------------------------------------------------------
-# Emma Child - Webpack Theme Entrypoint
---------------------------------------------------------------*/
-
-// CSS
-import './sass/theme.scss';
-
-// JS
-import '../../emma/src/js/emma-theme';
